62757cf1b591069089868c5b422792368abda10f
Fernando Cillero <fernando.cillero@a3sec.com>
2017-01-13 21:50:59 +0100

    Update admin.py

--- restapi/admin.py
+++ restapi/admin.py
@@ -1,18 +1,8 @@
 from django.contrib import admin
-from models import Place, Cluster, Beacon
+from models import Place
 
 
 class PlaceAdmin(admin.ModelAdmin):
     pass
 
-
-class ClusterAdmin(admin.ModelAdmin):
-    pass
-
-
-class BeaconAdmin(admin.ModelAdmin):
-    pass
-
 admin.site.register(Place, PlaceAdmin)
-admin.site.register(Cluster, ClusterAdmin)
-admin.site.register(Beacon, BeaconAdmin)
