0300b11836ec83450c9992b32d89a17361aa9277
Fernando Cillero <fernando.cillero@a3sec.com>
2017-01-13 21:49:41 +0100

    Update serializers

--- restapi/api/serializers.py
+++ restapi/api/serializers.py
@@ -1,20 +1,8 @@
 from rest_framework import serializers
-from ..models import Place, Cluster, Beacon
+from ..models import Place
 
 
 class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
         fields = ('name', 'lat', 'longi')
-
-
-class ClusterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cluster
-        fields = ('name', 'place')
-
-
-class BeaconSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Beacon
-        fields = ('name', 'cluster')
