a94842596895b713932a97e2796cbdfdbb5da6bb
Fernando Cillero <fernando.cillero@a3sec.com>
2017-02-19 21:20:17 +0100

    Add form to Places section

--- restapi/views.py
+++ restapi/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.views import View as ViewDjango
 from django.shortcuts import render
 from .models import Place
+from .forms import PlaceForm
 from .api.serializers import PlaceSerializer
 from rest_framework import generics, permissions
 from rest_framework.views import APIView, View
@@ -50,3 +52,23 @@ class PlacesViewIndex(ViewDjango):
 
     def get(self, request, *args, **kargs):
         return render(request, self.template_name)
+
+
+class PlacesFormView(ViewDjango):
+    template_name = "place-form.html"
+
+    def get(self, request, *args, **kwargs):
+        place_form = PlaceForm()
+        return render(request, self.template_name, {'form': place_form})
+
+    def post(self, request, *args, **kwargs):
+        place_form = PlaceForm(request.POST)
+        if place_form.is_valid():
+            Place.objects.create(
+                name=place_form.cleaned_data['name'],
+                description=place_form.cleaned_data['description'],
+                lat=place_form.cleaned_data['lat'],
+                longi=place_form.cleaned_data['longi']
+            )
+
+        return HttpResponseRedirect(reverse('place_index'))
